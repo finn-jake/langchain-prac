bdbae843da8c647f0f57c077a7630d3323ebc619
finn-jake <dongha17414@gmail.com>
2024-08-20 19:06:12 +0900

    0820 update

--- main_app.py
+++ main_app.py
@@ -252,7 +252,7 @@ def init_schat_session_state():
     st.title("🥑 Chat with GPT Version 2")  # 애플리케이션의 제목을 설정
     st.subheader(":blue[For Smart Cho]")  # 정보글을 출력
     st.write(":red[더 빨라지게 만듦]")
-    st.write(":red[오류 나는 거 고침]")
+    st.write(":red[오류 나는 거 고침 (옆에 슬라이더 아직 작동 안함)]")
     st.divider()
 
     # 모델 선택을 위한 selectbox 추가
